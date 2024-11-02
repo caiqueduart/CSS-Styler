72b2cf17c91cdfef1249ccabf36a3fc1f0363d51
caiqueduart <dcaique242@gmail.com>
2024-11-02 17:15:19 -0300

    Refatorações

--- scripts/button.js
+++ scripts/button.js
@@ -1,9 +1,8 @@
 const config = document.forms.buttonConfig;
 const button = document.querySelector('#model-button');
-let isDragging = false;
 
 function changeText(value) {
-    if(value) {
+    if (value) {
         button.innerText = value;
     } else {
         this.button.innerText = 'Resultado aqui!'
@@ -34,44 +33,20 @@ function changeBorderRadius(value) {
     button.style.borderRadius = value + 'px';
 }
 
-config.addEventListener('pointerdown', () => {
-    isDragging = true;
-});
-
-config.addEventListener('pointerup', () => {
-    isDragging = false;
-});
-
 config.text.addEventListener('keyup', (event) => {
-    const value = event.target.value
-    changeText(value);
+    changeText(event.target.value);
 });
 
-['pointermove', 'click'].forEach((action) => {
-    config.width.addEventListener(action, (event) => {
-
-        if(isDragging || action === 'click') {
-            changeWidth(event.target.value);
-        }
-    });
+config.width.addEventListener('input', (event) => {
+    changeWidth(event.target.value);
 });
 
-['pointermove', 'click'].forEach((action) => {
-    config.height.addEventListener(action, (event) => {
-        
-        if(isDragging || action === 'click') {
-            changeHeight(event.target.value);
-        }
-    });
+config.height.addEventListener('input', (event) => {
+    changeHeight(event.target.value);
 });
 
-['pointermove', 'click'].forEach((action) => {
-    config.borderRadius.addEventListener(action, (event) => {
-    
-        if(isDragging || action === 'click') {
-            changeBorderRadius(event.target.value);
-        }
-    });
+config.borderRadius.addEventListener('input', (event) => {
+    changeBorderRadius(event.target.value);
 });
 
 config.backgroundColor.addEventListener('input', (event) => {
